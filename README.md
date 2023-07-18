9cf398d2abef627e1005bb25b8919e266beea9a0
ihnaqi <ih.naqi@outlook.com>
2023-07-18 15:13:42 +0500

    fixed broken pieces

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ This project is a demo portfolio website which has been written using template e
 
 ## Project Structure
 
-Once cloning the project using `git clone https://github.com/ihnaqi/Portfolio-Website-Pug-Jade-.git` open folder <frontend> in the command line using `cd Frontend`, here you will see an `index.pug` file and some folders too which I have structured you don't need to worry about.
+Once cloning the project using `https://github.com/ihnaqi/ihnaqi.github.io.git` open folder in the command line using `cd Frontend`, here you will see an `index.pug` file and some folders too which I have structured you don't need to worry about.
 Once you are inside `Frontend` folder run this command in the terminal `pug -w ./ -o ./HTML -P`, this will make html file compiling the `.pug` files associated with the `index.pug` file. In order to run `pug watcher` `node` and `pug cli` must be installed on your computer. If you haven't installed both on your PC then follow the following links.
 
 > To install node js [Click here](https://nodejs.org/en/download/).
-> Once node is installed run this command `npm install pug-cli -g`
+> Once node is installed run this command ```npm install pug-cli -g```bash
 
 ## Screenshots of the complete project
 
--- js/script.js
+++ js/script.js
@@ -1,99 +1,172 @@
 var typed = new Typed(".typing", {
-  strings: [
-    "",
-    "Web Designer",
-    "Web Developer",
-    "Java Developer",
-    "React.js Developer",
-    "Node.js Developer",
-    "Flutter Developer",
-  ],
-  typeSpeed: 78,
-  BackSpeed: 60,
-  loop: true,
+   strings: [
+      "",
+      "Web Designer",
+      "Web Developer",
+      "Java Developer",
+      "React.js Developer",
+      "Node.js Developer",
+      "Flutter Developer",
+   ],
+   typeSpeed: 78,
+   BackSpeed: 60,
+   loop: true,
 });
 
 const nav = document.querySelector(".nav"),
-  navList = nav.querySelectorAll("li"),
-  totalNavList = navList.length,
-  allSection = document.querySelectorAll(".section"),
-  totalSection = allSection.length;
+   navList = nav.querySelectorAll("li"),
+   totalNavList = navList.length,
+   allSection = document.querySelectorAll(".section"),
+   totalSection = allSection.length;
 for (let i = 0; i < totalNavList; i++) {
-  const a = navList[i].querySelector("a");
-  a.addEventListener("click", function () {
-    removeBackSection();
-    for (let j = 0; j < totalNavList; j++) {
-      if (navList[j].querySelector("a").classList.contains("active")) {
-        allSection[j].classList.add("back-section");
+   const a = navList[i].querySelector("a");
+   a.addEventListener("click", function () {
+      removeBackSection();
+      for (let j = 0; j < totalNavList; j++) {
+         if (navList[j].querySelector("a").classList.contains("active")) {
+            allSection[j].classList.add("back-section");
+         }
+         addBackSection(j);
       }
-      addBackSection(j);
-    }
-    this.classList.add("active");
-    showSection(this);
-    if (window.innerWidth < 1100) {
-      asideSectionTogglerBtn();
-    }
-  });
+      this.classList.add("active");
+      showSection(this);
+      if (window.innerWidth < 1100) {
+         asideSectionTogglerBtn();
+      }
+   });
 }
 
 function addBackSection(index) {
-  navList[index].querySelector("a").classList.remove("active");
+   navList[index].querySelector("a").classList.remove("active");
 }
 
 function removeBackSection() {
-  for (let i = 0; i < totalSection; i++) {
-    allSection[i].classList.remove("back-section");
-  }
+   for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+   }
 }
 
 function showSection(element) {
-  for (let i = 0; i < totalSection; i++) {
-    allSection[i].classList.remove("active");
-  }
-  const target = element.getAttribute("href").split("#")[1];
-  document.querySelector("#" + target).classList.add("active");
+   for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("active");
+   }
+   const target = element.getAttribute("href").split("#")[1];
+   document.querySelector("#" + target).classList.add("active");
 }
 
 function updateNav(element) {
-  for (let i = 0; i < totalNavList; i++) {
-    navList[i].querySelector("a").classList.remove("active");
-    const target = element.getAttribute("href").split("#")[1];
-    if (
-      target == navList[i].querySelector("a").getAttribute("href").split("#")[1]
-    ) {
-      navList[i].querySelector("a").classList.add("active");
-    }
-  }
+   for (let i = 0; i < totalNavList; i++) {
+      navList[i].querySelector("a").classList.remove("active");
+      const target = element.getAttribute("href").split("#")[1];
+      if (
+         target == navList[i].querySelector("a").getAttribute("href").split("#")[1]
+      ) {
+         navList[i].querySelector("a").classList.add("active");
+      }
+   }
 }
 
-// document.querySelector(".hire-me").addEventListener("click", function () {
-//   const sectionIndex = this.getAttribute("data-section-index");
-//   showSection(this);
-//   updateNav(this);
-//   removeBackSection();
-//   addBackSection(sectionIndex);
-// });
-
 const navToggleBtn = document.querySelector(".nav-toggler"),
-  aside = document.querySelector(".aside");
-navToggleBtn.addEventListener("click", () => {
-  asideSectionTogglerBtn();
+   aside = document.querySelector(".aside");
+   navToggleBtn.addEventListener("click", () => {
+   asideSectionTogglerBtn();
 });
+
 function asideSectionTogglerBtn() {
-  aside.classList.toggle("open");
-  navToggleBtn.classList.toggle("open");
-  for (let i = 0; i < totalSection; i++) {
-    allSection[i].classList.toggle("open");
-  }
+   aside.classList.toggle("open");
+   navToggleBtn.classList.toggle("open");
+   for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.toggle("open");
+   }
 }
 
 document.querySelector("#cv-btn").addEventListener('click', () => {
-  var link = "https://drive.google.com/file/d/1OXUDLd4LKs7oEAS-RNTXLnFXv8MNAyJH/view?usp=drive_link"
-  var a = document.createElement("a");
-  a.href = link;
-  document.body.appendChild(a)
-  a.download = "Ishtiaq Naqi Resume.pdf";
-  a.target = "_blank"
-  a.click();
-  document.body.removeChild(a);
+   var link = "https://drive.google.com/file/d/1OXUDLd4LKs7oEAS-RNTXLnFXv8MNAyJH/view?usp=drive_link"
+   var a = document.createElement("a");
+   a.href = link;
+   document.body.appendChild(a)
+   a.download = "Ishtiaq Naqi Resume.pdf";
+   a.target = "_blank"
+   a.click();
+   document.body.removeChild(a);
 })
+
+const items = document.getElementsByClassName("portfolio-item");
+
+for (let i = 0; i < items.length; i++) {
+   switch (i) {
+      case 0:
+         items[0].addEventListener('click', () => {
+            const a = document.createElement("a")
+            a.href = 'https://ihnaqivans.netlify.app/'
+            document.body.appendChild(a)
+            a.click()
+            document.removeChild(a)
+         })
+         break;
+      case 1:
+         items[1].addEventListener('click', () => {
+            const a = document.createElement("a")
+            a.href = 'https://github.com/ihnaqi/ihnaqi.github.io'
+            document.body.appendChild(a)
+            a.click()
+            document.removeChild(a)
+         })
+         break;
+      case 2:
+         items[2].addEventListener('click', () => {
+            const a = document.createElement("a")
+            a.href = 'https://ihnaqitenzies.netlify.app/'
+            document.body.appendChild(a)
+            a.click()
+            document.removeChild(a)
+         })
+         break;
+      case 3:
+         items[3].addEventListener('click', () => {
+            const a = document.createElement("a")
+            a.href = 'https://github.com/ihnaqi/Hope_Foundation_Management_System'
+            document.body.appendChild(a)
+            a.click()
+            document.removeChild(a)
+         })
+         break;
+      case 4:
+         items[4].addEventListener('click', () => {
+            const a = document.createElement("a")
+            a.href = 'https://github.com/ihnaqi/'
+            document.body.appendChild(a)
+            a.click()
+            document.removeChild(a)
+         })
+         break;
+      case 5:
+         items[5].addEventListener('click', () => {
+            const a = document.createElement("a")
+            a.href = 'https://github.com/ihnaqi/Problem-Solving-Competitive-Programming-'
+            document.body.appendChild(a)
+            a.click()
+            document.removeChild(a)
+         })
+         break;
+   }
+}
+
+// Contact Me Form
+function contact() {
+   const sender = document.getElementById("email").value;
+   const subject = document.getElementById('subject').value;
+   const message = `${document.getElementById('name').value}\n\n${document.getElementById('message').value}`
+   console.log(`Sender: ${sender}\nSubject: ${subject}\nMessage: ${message}`)
+   Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "ih.spammable@gmail.com",
+      Password: "AB5E6228F4BE85D69F3BDA5023F39C1C32F7",
+      To: 'ihnaqi.dev@gmail.com',
+      From: sender,
+      Subject: subject,
+      Body: message
+   }).then(
+      message => alert(message)
+   );
+}
